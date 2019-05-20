83065dc634b6f9d87064a78dee9e054211288d2a
Lauren Eggers <neyabenz@gmail.com>
2019-05-20 13:59:54 -0400

    added random colors to js

--- colorGame.js
+++ colorGame.js
@@ -1,29 +1,52 @@
-let colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 0, 255)",
-    "rgb(0, 255, 255)",
-    "rgb(255, 0, 255)",
-]
+let colors = randomColors(6);
 
 let squares = document.getElementsByClassName("square");
-let pickedColor = colors[4];
+let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplayed");
+let messageResult = document.getElementById("result");
 
 colorDisplay.textContent = pickedColor;
 for(let ii = 0; ii < squares.length; ii++) {
     squares[ii].style.backgroundColor = colors[ii];
 
     squares[ii].addEventListener("click", (e) => {
-        //grab color of clicked square
         let clickedColor = e.currentTarget.style.backgroundColor;
-        //compare color
+
         if (clickedColor === pickedColor) {
-            console.log("correct")
+            messageResult.textContent = "Correct!";
+            winColor(pickedColor);
         } else {
-            console.log("wrong")
+            messageResult.textContent = "Try Again!";
+            e.currentTarget.style.backgroundColor = "rgb(44, 44, 44)";
         }
     });
 }
 
+function pickColor() {
+    let random = Math.floor(Math.random() * colors.length);
+    return colors[random]
+}
+function randomColors(n) {
+    let arr = []
+    //add n number of random colors to array
+    for (let kk = 0; kk < n; kk++){
+        let num1 = randomNum();
+        let num2 = randomNum();
+        let num3 = randomNum();
+        arr.push(`rgb(${num1}, ${num2}, ${num3})`)
+    }
+    return arr;
+}
+//randomize 0-255
+function randomNum() {
+    let min = Math.ceil(0);
+    let max = Math.floor(255);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//changes color of all squares
+function winColor(winColor) {
+    for (let jj = 0; jj < squares.length; jj++) {
+        squares[jj].style.backgroundColor = winColor;
+    }
+}
